1188708e90152a9f044dc10c3846093fa057a7a3
Nurik <psmartzhuawei@gmail.com>
2024-07-29 16:36:12 +0500

    changed the database design scheme and delete unnecessary script file

--- MAC-service/src/main/java/com/micro/pe/macservice/controller/CategoryController.java
+++ MAC-service/src/main/java/com/micro/pe/macservice/controller/CategoryController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -47,54 +46,3 @@ public class CategoryController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
-
-
-
-//package com.micro.pe.macservice.controller;
-//
-//import com.micro.pe.macservice.entity.Category;
-//import com.micro.pe.macservice.service.CategoryService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("api/s3/category")
-//public class CategoryController {
-//
-//    @Autowired
-//    private CategoryService categoryService;
-//
-//    @PostMapping("/create-category")
-//    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
-//        Category createdCategory = categoryService.createCategory(category);
-//        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("/getAllCategory")
-//    public ResponseEntity<List<Category>> getAllCategory() {
-//        List<Category> categories = categoryService.getAllCategories();
-//        return new ResponseEntity<>(categories, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/getCategoryByID/{id}")
-//    public ResponseEntity<Category> getCategoryByID(@PathVariable("id") int id) {
-//        Category category = categoryService.getCategoryById(id);
-//        return new ResponseEntity<>(category, HttpStatus.OK);
-//    }
-//
-//    @PutMapping("/updateCategory/{id}")
-//    public ResponseEntity<Category> updateCategory(@PathVariable("id") int id, @RequestBody Category category) {
-//        Category updatedCategory = categoryService.updateCategory(id, category);
-//        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/deleteCategory/{id}")
-//    public ResponseEntity<Category> deleteCategory(@PathVariable("id") int id) {
-//        categoryService.deleteCategoryById(id);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
-//
-//}
